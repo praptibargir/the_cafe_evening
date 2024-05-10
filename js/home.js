de446bab3203f175103dffe0deff7d24100ed7f5
praptibargir <praptibargir21@gmail.com>
2024-05-10 18:20:46 +0530

    gallery added and menu page updated

--- js/home.js
+++ js/home.js
@@ -10,7 +10,7 @@ function darkMode() {
 
 function img1() {
     const slider = document.getElementById("slider");
-    slider.innerHTML = '<div class="card-group" id="slider"><div class="card"><img src="images/slider1.jpg" class="card-img-top" alt="..."></div><div class="card"></div><div class="card"></div></div>'
+    slider.innerHTML = '<div class="card-group" id="slider"><div class="card"><img src="images/slider1.jpg" class="img1 card-img-top" alt="..."></div></div>'
 }
 
 function img2() {
@@ -25,4 +25,34 @@ function img3() {
 setTimeout(img1, 3000);
 setTimeout(img2, 6000);
 setTimeout(img3, 9000);
+
+function gall1(){
+    const image=document.getElementById("mainimg");
+    image.src="../images/people2.jpg";
+}
+
+function gall2(){
+    const image=document.getElementById("mainimg");
+    image.src="../images/people1.jpg";
+}
+
+function gall3(){
+    const image=document.getElementById("mainimg");
+    image.src="../images/test1.jpg";
+}
+
+function gall4(){
+    const image=document.getElementById("mainimg");
+    image.src="../images/people4.jpg";
+}
+
+function gall5(){
+    const image=document.getElementById("mainimg");
+    image.src="../images/people3.jpg";
+}
+
+function gall6(){
+    const image=document.getElementById("mainimg");
+    image.src="../images/test2.jpg";
+}
 
--- js/menu.js
+++ js/menu.js
@@ -7,3 +7,14 @@ function darkMode() {
     const theme = document.getElementById("body");
     theme.className = "theme-dark";
 }
+
+function offer() {
+    let span = document.getElementById('offer');
+    if (span.style.display === 'none') {
+      span.style.display = 'inline';
+    } else {
+      span.style.display = 'none'; 
+    }
+  }
+  
+  setInterval(offer, 1000);
